19c7b7897d433ef5b403762099cec93014a63411
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2023-10-22 18:32:53 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count of smaller elements/count-of-smaller-elements.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            long k = Long.parseLong(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            System.out.println(obj.countOfElements(a, n, k));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long countOfElements(long arr[], long n, long x)
+    {  long count=0;
+        LinkedHashMap<Long,Long> mapset = new LinkedHashMap<>();
+        for(long i:arr)
+        {
+           mapset.put(i,mapset.getOrDefault(i,0L)+1L);
+        }
+         for (Map.Entry<Long, Long> entry : mapset.entrySet())
+        {
+            if(entry.getKey()<=x)
+            {
+         count=count+entry.getValue();
+            }
+        }
+        
+        return count;
+    }
+}
