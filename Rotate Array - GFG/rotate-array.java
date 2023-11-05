412c8abe18be084992564f7d976d318e34c0dae2
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2023-11-05 13:21:04 +0530

    Added solution - LeetHub

--- Rotate Array - GFG/rotate-array.java
+++ Rotate Array - GFG/rotate-array.java
@@ -55,21 +55,22 @@ class Solution
     //Function to rotate an array by d elements in counter-clockwise direction. 
     static void rotateArr(int arr[], int d, int n)
     {
-        d=d%n;
-        reverse(arr,0,d-1);
-        reverse(arr,d,n-1);
-        reverse(arr,0,n-1);
+          
+
+    d = d % n;
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, n - d - 1);
+    reverse(arr, n - d, n - 1);
+}
+static void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
     }
-    
-    static void reverse(int arr[], int start,int end){
-        while(start<end){
-            
-            int temp=arr[start];
-            arr[start]=arr[end];
-            arr[end]=temp;
-            start++;
-            end--;
-        } 
+}
         
-    }
+    
 }
