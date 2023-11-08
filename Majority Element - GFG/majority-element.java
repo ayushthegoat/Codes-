7bcafc98bb2813408a6fbfef7b62ca00ab1fd347
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2023-11-08 16:46:25 +0530

    Added solution - LeetHub

---
+++ Majority Element - GFG/majority-element.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+    Map<Integer,Integer>map=new LinkedHashMap<>();
+    for(int i:a){
+        map.put(i,map.getOrDefault(i,0)+1);
+    }
+    for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+        if(entry.getValue()>(size/2))
+        {
+            return entry.getKey();
+        }
+    }
+    return -1;
+    }
+}
