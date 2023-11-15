2c4166d052d6048609530324b95d7d27eac3ba08
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2023-11-15 18:13:44 +0530

    Added solution - LeetHub

--- Count pairs with given sum - GFG/count-pairs-with-given-sum.java
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.java
@@ -32,31 +32,21 @@ public class GFG {
 
 class Solution {
     int getPairsCount(int[] arr, int n, int k) {
-        
-        
-       Map<Integer,Integer> map=new HashMap<>();
-       int sum=0;
-       int count=0;
-       for(int i=0;i<n;i++){
-           if(map.containsKey(k-arr[i])){
-               count=count+map.get(k-arr[i]);
-           }
-               
-           
-           map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-       }
-       return count;
+        // code here
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int count=0;
+    for(int i=0;i<n;i++){
+        if(map.containsKey(k-arr[i])){
+            count=count+map.get(k-arr[i]);
+            if(k-arr[i]==arr[i]){
+                count--;
+            }
+        }
+       
+    }
+    return count/2; 
+    }
 }
-}
-
-
-//   Map<Integer,Integer> map = new HashMap<>();
-//         int sum = 0;
-//         int count = 0;
-//         for(int i=0;i<n;i++){
-//             if(map.containsKey(k-arr[i])){
-//                 count+=map.get(k-arr[i]);
-//             }
-//             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-//         }
-//         return count;
