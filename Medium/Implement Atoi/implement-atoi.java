ed662351b4284618b87e073ff2d82d9cafb59233
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2023-11-17 18:27:49 +0530

    Updated Solution - GfG to GitHub

--- Medium/Implement Atoi/implement-atoi.java
+++ Medium/Implement Atoi/implement-atoi.java
@@ -31,12 +31,41 @@ class aToi
 class Solution
 {
     int atoi(String str) {
-        int res=-1;
-        try{
-            res=Integer.parseInt(str);
-        }catch(NumberFormatException e){
-            res=-1;
+        boolean neg=false;
+        boolean alphaPresent=false;
+        if(str==null || str.length()==0){return 0;}
+        
+        str=str.trim();
+        if(str.charAt(0)=='-'){
+            neg=true;
+             str=str.substring(1,str.length());
         }
-        return res;
+       
+        int result=0;
+        int beg=0;
+        while(beg<str.length()){
+         char c=str.charAt(beg);
+            if(c>='0' && c<='9'){
+            result=result*10+(c-'0');
+            }else if(str.charAt(beg)!='0' ||str.charAt(beg)!='1'||str.charAt(beg)!='2'
+            ||str.charAt(beg)!='3'||str.charAt(beg)!='4'||str.charAt(beg)!='5'||str.charAt(beg)!='6'
+            ||str.charAt(beg)!='7'||str.charAt(beg)!='8'||str.charAt(beg)!='9'){
+            alphaPresent=true;
+            
+           
+           
+        }
+        beg++;
+        }
+        if(alphaPresent==true){
+            return -1;
+        }
+           if(neg==true){result=result*(-1);return result;}
+     
+        
+    
+    return result;
     }
 }
+    
+
