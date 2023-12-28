cb81b361e62dc9c1a21feb7be63c8535657b3542
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2023-12-29 01:19:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findSingle(N, arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int findSingle(int N, int arr[]){
+        // code here
+    Map<Integer,Integer>map=new HashMap<>();
+    for(int i:arr)map.put(i,map.getOrDefault(i,0)+1);
+    for(int i:map.keySet()){if(map.get(i)==1)return i;}
+        return 0;
+    }
+}
