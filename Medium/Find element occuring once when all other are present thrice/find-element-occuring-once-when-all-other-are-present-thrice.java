e7ed99a9ed71d4ebddf7122551ebc39cf9813661
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-01-03 11:01:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            int[] arr = new int[N];
+            
+            for(int i=0 ; i<N ; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.singleElement(arr,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int singleElement(int[] arr , int N) {
+        // code here
+         HashMap<Integer,Integer> map1=new HashMap<>();
+        
+       for (int i = 0; i < N; i++) {
+            map1.put(arr[i], map1.getOrDefault(arr[i], 0) + 1);
+        }
+
+         int lessFrequency=0;
+        for(int element:map1.keySet())
+        {
+            int val=map1.get(element);
+            
+            if(val<3)
+            {
+                lessFrequency=element;
+                break;
+            }
+        }
+        return lessFrequency;
+       
+        
+    }
+}
