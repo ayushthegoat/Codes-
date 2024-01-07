b3170ac1803944c812bc71240f9010b5e9011e75
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-01-08 01:25:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count triplets with sum smaller than X/count-triplets-with-sum-smaller-than-x.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+class Solution
+{
+    long countTriplets(long arr[], int n,int X)
+    {
+            Arrays.sort(arr);
+            
+            
+            long ctr=0;
+            for(int i=0;i<n-1;i++){
+                int low=i+1;
+                int high=n-1;
+                while(low<high){
+                    long sum=arr[i]+arr[low]+arr[high];
+                      if(sum<X){
+                          ctr+=(high-low);
+                          low++;
+                      }else{
+                          high--;
+                      }
+                }
+            }
+            return ctr;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int k =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+//            String line2 = br.readLine();
+//            String[] a2 = line2.trim().split("\\s+");
+//            long b[] = new long[m];
+//            for (int i = 0; i < m; i++) {
+//                b[i] = Long.parseLong(a2[i]);
+//            }
+            Solution ob = new Solution();
+            long ans=ob.countTriplets(a,n,k);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
