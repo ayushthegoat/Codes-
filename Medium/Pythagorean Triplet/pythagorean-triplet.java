d17aac5ccd3421e51aa24e60374d36d8daad0938
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-01-09 01:41:42 +0530

    Updated Solution - GfG to GitHub

--- Medium/Pythagorean Triplet/pythagorean-triplet.java
+++ Medium/Pythagorean Triplet/pythagorean-triplet.java
@@ -34,22 +34,29 @@ public class Main {
 class Solution {
     boolean checkTriplet(int[] arr, int n) {
         // code here
-       Set<Integer> squares = new HashSet<>();
-    
-    
-    for (int i = 0; i < n; i++) {
-        squares.add(arr[i] * arr[i]);
-        
-        
-    }
-    
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            int sqsum = (arr[i]*arr[i])+(arr[j]*arr[j]);
-            if(squares.contains(sqsum))
-            return true;
+   if(n<3){
+	        return false;
+	    }
+	    Arrays.sort(arr);
+	    for(int i=n-1;i>=2;i--){
+	        int sum1=arr[i]*arr[i];
+	        int a=0,b=i-1;
+	        while(a<b){
+	            if(arr[i]==arr[i-1]){
+	                break;
+	            }
+	            int sum2=(arr[a]*arr[a])+(arr[b]*arr[b]);
+	            if(sum1==sum2){
+	                return true;
+	            }
+	            else if(sum1>sum2){
+	                a++;
+	            }
+	            else{
+	                b--;
+	            }
+	        }
+	    }
+	    return false;
         }
-    }
-    return false;
-    }
 }
