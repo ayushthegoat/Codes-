93c1d3c6f2d8f2edc5ec38d13aa23fa371e63dbc
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-02-08 00:29:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Plus One/plus-one.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            ArrayList<Integer> arr = new ArrayList<>();
+            
+            for(int i=0 ; i<N ; i++)
+                arr.add(Integer.parseInt(S[i]));
+
+            Solution ob = new Solution();
+            ArrayList<Integer> res = ob.increment(arr,N);
+            
+            for(int i=0; i<res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static ArrayList<Integer> increment(ArrayList<Integer> list , int N) {
+        // code here
+        int carry=1;
+        int total=0;
+        for(int i=list.size()-1;i>=0;i--){
+            total=list.get(i)+carry;
+            list.set(i,total%10);
+            carry=total/10;
+            
+        }
+        if(carry>0){
+            list.add(0,carry);
+        }
+        return list;
+    }
+};
