c1f7917fdbd276a6e7744822c01e5ad1d40c4cfb
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-02-09 01:10:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Next Greater Element/next-greater-element.java
+++ Medium/Next Greater Element/next-greater-element.java
@@ -9,44 +9,47 @@ class GFG {
     
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
 		int t = Integer.parseInt(br.readLine().trim());
 		while(t-->0){
 		    int n = Integer.parseInt(br.readLine().trim());
 		    String inputLine[] = br.readLine().trim().split(" ");
 		    long[] arr = new long[n];
 		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    long[] res = new Solution().nextLargerElement(arr, n);
+		    Solution ob = new Solution();
+		    long[] res = ob.nextLargerElement(arr, n);
 		    for (int i = 0; i < n; i++) 
-		        System.out.print(res[i] + " ");
-		    System.out.println();
+		        ot.print(res[i] + " ");
+		    ot.println();
 		}
+        ot.close();
 	}
 }
-
 
 
 
 // } Driver Code Ends
 
 
-class Solution
-{
-    //Function to find the next greater element for each element of the array.
-    public static long[] nextLargerElement(long[] arr, int n)
-    { 
+// User Function Template for JAVA
+
+class Solution{
+    public static long[] nextLargerElement(long[] arr, int n) { 
         // Your code here
-        long[] res=new long[n];
+        long[] result=new long[n];
+        Arrays.fill(result,-1);
         Stack<Long>stk=new Stack<>();
-        Arrays.fill(res,-1);
+        
         for(int i=n-1;i>=0;i--){
-           while(!stk.isEmpty() && arr[i]>=stk.peek()){
+            while(!stk.isEmpty() && arr[i]>=stk.peek()){
                 stk.pop();
-           }
-               if(!stk.isEmpty()){
-                   res[i]=stk.peek();
-               }
-               stk.push(arr[i]);
+            }
+            if(!stk.isEmpty()){
+                result[i]=stk.peek();
+            }
+            stk.push(arr[i]);
+            
         }
-        return res;
+        return result;
     } 
 }
