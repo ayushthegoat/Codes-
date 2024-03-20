d3c35ef509f685c1799c1621643de19b66865b72
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-03-20 19:17:59 +0530

    Time: 9 ms (95.01%) | Memory: 44.1 MB (96.98%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,23 +1,33 @@
 class Solution {
     public int maxVowels(String s, int k) {
-    Set<Character>set=new HashSet<>();
-    set.add('a');set.add('e');set.add('i');set.add('o');set.add('u');
+     int[] vowels=new int[26];
+     vowels['a'-'a']=1;
+     vowels['e'-'a']=1;
+     vowels['i'-'a']=1;
+     vowels['o'-'a']=1;
+     vowels['u'-'a']=1;
+     
+     int maxVowels=0;
+     int curr=0;
 
-      int maxVowels=0;
-      int currCount=0;
+     for(int i=0;i<k;i++){
+       char ch=s.charAt(i);
+       if(vowels[ch-'a']==1)curr++;
+     }
+     maxVowels=curr;
 
-      for(int i=0;i<k;i++){
-        if(set.contains(s.charAt(i)))currCount++;
-      }
-      maxVowels=currCount;
-      for(int i=k;i<s.length();i++){
-        if(set.contains(s.charAt(i-k)))currCount--;
+    for(int i=k;i<s.length();i++){
+        char chA=s.charAt(i);
+        char chR=s.charAt(i-k);
+        if(vowels[chR-'a']==1)curr--;
 
-        if(set.contains(s.charAt(i)))currCount++;
+        if(vowels[chA-'a']==1)curr++;
 
-        maxVowels=Math.max(maxVowels,currCount);
-      }
-        
+        maxVowels=Math.max(maxVowels,curr);
+    }
+
+
+
         return maxVowels;
     }
 }
