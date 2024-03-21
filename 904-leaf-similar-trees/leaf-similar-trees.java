f3533a53356cb0d415e7b86dddbafccbbc3b1187
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-03-21 22:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (53.18%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -14,30 +14,30 @@
  * }
  */
 class Solution {
-    public boolean leafSimilar(TreeNode r1, TreeNode r2) {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
        
-         ArrayList<Integer> arr1=new ArrayList<>();
-        ArrayList<Integer> arr2=new ArrayList<>();
-        calcLeaf(r1,arr1);
-        calcLeaf(r2,arr2);
+        ArrayList<Integer> t1 = new ArrayList<>();
+        ArrayList<Integer> t2 = new ArrayList<>();
+        fillLists(root1,t1);
+        fillLists(root2,t2);
         int j=0;
-        if(arr1.size()!=arr2.size())return false;
-        for(int i:arr1){
-            if(i!=arr2.get(j++)){
+        if(t1.size()!=t2.size())return false;
+        for(int i:t1){
+            if(i!=t2.get(j++)){
                 return false;
             }
         }
         return true;
     }
-    public void calcLeaf(TreeNode n,ArrayList<Integer> arr){
-        if(n==null){
-            return;
+    public void fillLists(TreeNode root,ArrayList<Integer>list){
+        if(root==null){
+            return ;
         }
-        if(n.left==null&&n.right==null){
-            arr.add(n.val);
-            return;
+        if(root.left==null && root.right==null){
+            list.add(root.val);
+            return ;
         }
-        calcLeaf(n.left,arr);
-        calcLeaf(n.right,arr);
+        fillLists(root.left,list);
+        fillLists(root.right,list);
     }
 }
