cac612a4adc817a0699056c654835ddc73e200aa
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-03-24 07:43:06 +0530

    Time: 43 ms (13.42%) | Memory: 55.5 MB (87.72%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -3,18 +3,12 @@ class Solution {
         int count=0;
         Map<Integer , Integer> map = new HashMap();
         for(int i=0;i<nums.length;i++){
-            int temp=k-nums[i];
-            if(map.containsKey(temp)){
-                count++;
-            
-            if(map.get(temp)==1){
-                map.remove(temp);
-            }else{
-                map.put(temp,map.get(temp)-1);
-            }
-            }else{
-            map.put(nums[i],map.getOrDefault(nums[i] , 0 ) + 1 );
-            }
+           if(map.containsKey(k-nums[i]) && map.get(k-nums[i])>0){
+              count++;
+              map.put(k-nums[i],map.get(k-nums[i])-1);
+           }else{
+              map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+           }
         }
         return count;
     }
