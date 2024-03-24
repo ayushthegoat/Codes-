1fc860879fa296236da4487d6084f201b6a1e306
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-03-24 16:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 50.8 MB (82.81%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -17,17 +17,27 @@ class Solution {
     int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root==null)return 0;
-        Queue<TreeNode>q=new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode temp=q.poll();
-            if(temp.val>=low && temp.val<=high)sum+=temp.val;
+        // Queue<TreeNode>q=new LinkedList<>();
+        // q.offer(root);
+        // while(!q.isEmpty()){
+        //     TreeNode temp=q.poll();
+        //     if(temp.val>=low && temp.val<=high)sum+=temp.val;
 
-            if(temp.left!=null)q.offer(temp.left);
+        //     if(temp.left!=null)q.offer(temp.left);
 
-            if(temp.right!=null)q.offer(temp.right);
+        //     if(temp.right!=null)q.offer(temp.right);
+        // }
+
+        int sum=0;
+        if(root.val>low){
+           sum+=rangeSumBST(root.left,low,high);
         }
-
+        if(root.val>=low && root.val<=high){
+            sum+=root.val;
+        }
+        if(root.val<high){
+            sum+=rangeSumBST(root.right,low,high);
+        }
         return sum;
     }
 }
