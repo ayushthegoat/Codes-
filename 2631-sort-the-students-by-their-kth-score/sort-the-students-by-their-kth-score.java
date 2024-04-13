b521b03a914d95b61471c7522af8da2d0c93c8ed
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-04-13 19:37:04 +0530

    Time: 2 ms (95.62%) | Memory: 52.2 MB (40.29%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -2,19 +2,21 @@ class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
         
 
-        PriorityQueue<Integer> q = new PriorityQueue<>((a,b)->score[b][k]- score[a][k]);
-        for(int i=0;i<score.length;i++){
-            q.offer(i);
-        }
+        // PriorityQueue<Integer> q = new PriorityQueue<>((a,b)->score[b][k]- score[a][k]);
+        // for(int i=0;i<score.length;i++){
+        //     q.offer(i);
+        // }
       
-        int[][] res = new int[score.length][score[0].length];
-        int idx=0;
+        // int[][] res = new int[score.length][score[0].length];
+        // int idx=0;
         
-        while(!q.isEmpty()){
-            int index = q.poll();
-            res[idx++] = score[index];
-        }
+        // while(!q.isEmpty()){
+        //     int index = q.poll();
+        //     res[idx++] = score[index];
+        // }
+
+        Arrays.sort(score , (a,b)->b[k]-a[k]);
         
-        return res;
+        return score;
     }
 }
