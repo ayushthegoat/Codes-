698a8b4165b43a73c9bf0a26205442c228f86d0f
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-04-19 22:43:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse Spiral Form of Matrix/reverse-spiral-form-of-matrix.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> reverseSpiral(int R, int C, vector<vector<int>>&a)  {
+        // code here
+         int top = 0;
+        int bottom  = a.size()-1;
+        int left  = 0 ;
+        int right = a[0].size()-1;
+        vector<int> res;
+        
+        while(top<=bottom && left<=right){
+            
+            for(int i=left;i<=right;i++){
+                res.push_back(a[top][i]);
+            }
+            top++;
+            
+             for(int i=top;i<=bottom;i++){
+                res.push_back(a[i][right]);
+            }
+            right--;
+            
+            if(top<=bottom){
+             for(int i=right;i>=left;i--){
+                 res.push_back(a[bottom][i]);
+              }
+            }
+            bottom--;
+            
+            if(left<=right){
+             for(int i=bottom;i>=top;i--){
+                res.push_back(a[i][left]);
+            
+                }
+             }
+            left++;
+            
+        }
+        reverse(res.begin() , res.end());
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int R, C;
+        cin>>R>>C;
+        vector<vector<int>> a(R, vector<int>(C, 0));
+        for(int i = 0;i < R*C;i++)
+            cin>>a[i/C][i%C];
+        Solution ob;
+        vector<int>ans=ob.reverseSpiral(R,C,a);
+        for(int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+            cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
