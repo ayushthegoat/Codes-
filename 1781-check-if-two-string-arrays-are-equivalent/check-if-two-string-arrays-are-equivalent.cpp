e73d570db5f9080ad658debbd71120102fea0d88
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-04-26 22:04:19 +0530

    Time: 9 ms (7.48%) | Memory: 13.2 MB (76.93%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -3,10 +3,10 @@ public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string str1="";
         string str2="";
-        for(auto &iterator : word1){
+        for(string &iterator : word1){
             str1+=iterator;
         }
-        for(auto &iterator : word2){
+        for(string &iterator : word2){
             str2+=iterator;
         }
         return str1==str2;
