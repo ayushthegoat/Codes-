d8411b96081c8dae0e0dbfcbaa72414c5405f8fd
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-04-28 01:56:35 +0530

    Time: 244 ms (18.01%) | Memory: 164.8 MB (40.68%) - LeetSync

---
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int mod = 12345;
+
+        long long product = 1;
+        vector<vector<int>> gridRes(row, vector<int>(col));
+         vector<vector<int>> hash1(row, vector<int>(col));
+        vector<vector<int>> hash2(row, vector<int>(col));
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                hash1[i][j] = product;
+                product = product * grid[i][j];
+                product = product % mod;
+            }
+        }
+        product = 1;
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                hash2[i][j] = product;
+                product = product * grid[i][j];
+                product = product % mod;
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                long long mul = (hash1[i][j] * hash2[i][j]);
+                gridRes[i][j] = mul%mod;
+            }
+        }
+        return gridRes;
+    }
+};
