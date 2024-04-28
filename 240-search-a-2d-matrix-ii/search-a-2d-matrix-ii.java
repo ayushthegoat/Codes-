6c577160214c8da121eff0ffb59e1e1f6848c880
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-04-28 22:20:05 +0530

    Time: 6 ms (47.10%) | Memory: 46.4 MB (5.68%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -18,11 +18,10 @@ class Solution {
             int mid = left+(right-left)/2;
 
             if(arr[mid]==target)return true;
-            else if(arr[mid]>target){
-                right = mid-1;
-            }else{
-                left = mid+1;
-            }
+            else if(arr[mid]>target) right = mid-1;
+            else  left = mid+1;
+               
+            
         }
         return false;
     }
