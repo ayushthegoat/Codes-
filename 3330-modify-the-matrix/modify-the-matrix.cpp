dbcadb55304887ffd94bef24f5a49b310e5edd5b
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-04-28 18:08:52 +0530

    Time: 11 ms (56.20%) | Memory: 22.4 MB (23.77%) - LeetSync

--- 3330-modify-the-matrix/modify-the-matrix.cpp
+++ 3330-modify-the-matrix/modify-the-matrix.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
-         int row = matrix.size();
-    int col = matrix[0].size();
-    vector<int> store(col, INT_MIN); // Store the maximum value in each column
-
-    // Find the maximum value in each column
-    for (int i = 0; i < col; i++) {
-        for (int j = 0; j < row; j++) {
-            store[i] = max(store[i], matrix[j][i]);
-        }
-    }
-
-    // Create a new matrix to store the modified values
-    vector<vector<int>> answer(row, vector<int>(col));
-
-    // Replace -1 with the maximum value in its respective column in the new matrix
-    for (int i = 0; i < col; i++) {
-        for (int j = 0; j < row; j++) {
-            if (matrix[j][i] == -1) {
-                answer[j][i] = store[i];
-            } else {
-                answer[j][i] = matrix[j][i];
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<int>store(col,INT_MIN);
+        vector<vector<int>>answer(row,vector<int>(col));
+        
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row;j++){
+            store[i] = max(store[i],matrix[j][i]);
             }
         }
-    }
+        for(auto it:store)cout<<it;
 
-    return answer;
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row;j++){
+                if(matrix[j][i]==-1){
+                    answer[j][i] = store[i];
+                }else{
+                    answer[j][i] = matrix[j][i];
+                }
+            }
+        }
+        return answer;
     }
 };
