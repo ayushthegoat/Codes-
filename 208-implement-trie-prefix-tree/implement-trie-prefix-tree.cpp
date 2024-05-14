f15c2a2396fcb2d8303c2f334b04499016d65ed5
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-05-14 21:36:06 +0530

    Time: 41 ms (86.36%) | Memory: 48.1 MB (80.05%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,69 @@
+class Trie {
+public:
+    struct trieNode {
+        bool isEndOfWord;
+        trieNode* children[26];
+    };
+    trieNode* getNode() {
+        trieNode* newNode = new trieNode();
+        newNode->isEndOfWord = false;
+        for (int i = 0; i < 26; i++) {
+            newNode->children[i] = nullptr;
+        }
+        return newNode;
+    }
+    trieNode* root;
+    Trie() { root = getNode(); }
+
+    void insert(string word) {
+        trieNode* crawler = root;
+        for (const char& ch : word) {
+            if (crawler->children[ch - 'a'] == nullptr) {
+                crawler->children[ch - 'a'] = getNode();
+            }
+            crawler = crawler->children[ch - 'a'];
+        }
+        crawler->isEndOfWord = true;
+    }
+
+    bool search(string word) {
+        trieNode* crawler = root;
+        for (const char& ch : word) {
+            if (crawler->children[ch - 'a'] == nullptr) {
+                return false;
+            }
+            crawler = crawler->children[ch - 'a'];
+        }
+        if (crawler != nullptr && crawler->isEndOfWord == true) {
+            return true;
+        }
+        return false;
+    }
+
+    bool startsWith(string prefix) {
+        trieNode* crawler = root;
+        int i;
+        for (i = 0; i < prefix.size(); i++) {
+            char ch = prefix[i];
+            int idx = ch - 'a';
+
+            if (crawler->children[idx] == nullptr) {
+                return false;
+            }
+
+            crawler = crawler->children[idx];
+        }
+        if (i == prefix.size())
+            return true;
+        
+        return false;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
