31665cf8a470a62760648a30014c6f6151706e42
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-05-19 11:36:35 +0530

    Time: 15 ms (69.57%) | Memory: 16.8 MB (88.47%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int>adj[n];  
+
+        for(int i=0;i<pre.size();i++){
+            int u = pre[i][0];
+            int v = pre[i][1];
+            adj[u].push_back(v);
+        }
+
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<adj[i].size();j++){
+                indegree[adj[i][j]]++;
+            }
+        }
+
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int>result;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            result.push_back(node);
+                 
+                 for(auto it : adj[node]){
+                    indegree[it]--;
+
+                    if(indegree[it]==0){
+                        q.push(it);
+                    }
+                 }
+        }
+        if(result.size() != n) return {};
+        reverse(result.begin(), result.end());
+       return result;
+    }
+};
