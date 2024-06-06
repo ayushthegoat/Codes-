64cd9add49eebcc765664bac0c295c07df217b70
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-06 08:08:03 +0530

    Time: 18 ms (58.13%) | Memory: 18.8 MB (8.11%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-bool dfs( vector<int>adj[], int node, vector<int>&vis){
+    bool dfs(vector<int> adj[], int node, vector<int>& vis) {
 
-            for(auto it : adj[node]){
-                if(vis[it] == -1){
-                    vis[it] = !vis[node];
-                    if(!dfs(adj, it, vis))return false;
-                }else if(vis[it] == vis[node])return false;
-            }
-            return true;
-}
+        for (auto it : adj[node]) {
+            if (vis[it] == -1) {
+                vis[it] = !vis[node];
+                if (!dfs(adj, it, vis))
+                    return false;
+            } else if (vis[it] == vis[node])
+                return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
+        ios_base::sync_with_stdio(0), cin.tie(0);
         int n = graph.size();
-        vector<int>adj[n];
-        for(int i=0;i<n;i++){
-            for(auto it:graph[i]){
+        vector<int> adj[n];
+        for (int i = 0; i < n; i++) {
+            for (auto it : graph[i]) {
                 adj[i].push_back(it);
             }
         }
-        vector<int>vis(n,-1);
-        for(int i=0;i<n;i++){
-            if(vis[i]==-1){
+        vector<int> vis(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == -1) {
                 vis[i] = 0;
-                
-                    if(!dfs(adj, i, vis))return false;
-                }
+
+                if (!dfs(adj, i, vis))
+                    return false;
             }
-        
+        }
+
         return true;
     }
 };
