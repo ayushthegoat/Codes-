0d3204f80d6e6417cddfce9067ef305fa8ebfc9a
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-08 11:21:32 +0530

    Time: 126 ms (33.01%) | Memory: 89 MB (21.87%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,69 +1,36 @@
 class Solution {
 public:
-    vector<int> previous_smaller(vector<int>& heights){
+    vector<int> previous_smaller(vector<int>& heights) {
         int n = heights.size();
-        vector<int>res;
-        stack<int>stk;
+        vector<int> res;
+        stack<int> stk;
 
-        for(int i=0;i<n;i++){
-
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
                 stk.pop();
             }
-            int ele = stk.empty()?-1:stk.top();
+            int ele = stk.empty() ? -1 : stk.top();
             res.push_back(ele);
             stk.push(i);
-
         }
         return res;
     }
+
     vector<int> next_smaller(vector<int>& heights) {
         int n = heights.size();
-        vector<int>res;
-        stack<int>stk;
+        vector<int> res(n); // Initialize with the size of heights
+        stack<int> stk;
 
-        for(int i=n-1; i>=0; i--) {
-            while(!stk.empty() && heights[stk.top()] >= heights[i]) {
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
                 stk.pop();
             }
-            int ele = stk.empty()?n:stk.top();
-            res.push_back(ele);
+            res[i] = stk.empty() ? n : stk.top();
             stk.push(i);
         }
-        reverse(res.begin(), res.end());
         return res;
-     }
+    }
 
-    // vector<int> previous_smaller(vector<int>& heights) {
-    //     int n = heights.size();
-    //     vector<int> res;
-    //     stack<int> stk;
-
-    //     for (int i = 0; i < n; i++) {
-    //         while (!stk.empty() && heights[stk.top()] >= heights[i]) {
-    //             stk.pop();
-    //         }
-    //         int ele = stk.empty() ? -1 : stk.top();
-    //         res.push_back(ele);
-    //         stk.push(i);
-    //     }
-    //     return res;
-    // }
-
-    // vector<int> next_smaller(vector<int>& heights) {
-    //     int n = heights.size();
-    //     vector<int> res(n); // Initialize with the size of heights
-    //     stack<int> stk;
-
-    //     for (int i = n - 1; i >= 0; i--) {
-    //         while (!stk.empty() && heights[stk.top()] >= heights[i]) {
-    //             stk.pop();
-    //         }
-    //         res[i] = stk.empty() ? n : stk.top();
-    //         stk.push(i);
-    //     }
-    //     return res;
-    // }
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         vector<int> prev = previous_smaller(heights);
@@ -75,5 +42,26 @@ public:
             mx = max(mx, calc);
         }
         return mx;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty())
+            return 0;
+
+        int maxi = 0;
+        vector<int> height(matrix[0].size(), 0);
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < height.size(); j++) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            maxi = max(maxi, largestRectangleArea(height));
+        }
+
+        return maxi;
     }
 };
