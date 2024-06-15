1f32e0a4e3f82609a919c95d32ad28e4bd1aefe1
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-15 17:49:13 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subsets/subsets.cpp
+++ Medium/Subsets/subsets.cpp
@@ -1,72 +1,64 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
-class Solution
-{
-    public:
-    vector<vector<int> > subsets(vector<int>& A)
-    {
-        //code here
-        vector<vector<int>>ans;
-        vector<int>temp;
-        
-        solve(A,ans,temp,0);
-        sort(ans.begin(),ans.end());
-        return ans;
-    }
-    void solve(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int index){
-        if(index>arr.size()-1){
-            ans.push_back(temp);
+class Solution {
+  public:
+    void solve(vector<vector<int>>&res,  vector<int>&temp,  vector<int>& a, int index) {
+        if(index > a.size()-1) {
+            res.push_back(temp);
             return;
         }
-        temp.push_back(arr[index]);
-        solve(arr,ans,temp,index+1);
-        temp.pop_back();
-        solve(arr,ans,temp,index+1);
         
+        temp.push_back(a[index]);
+        solve(res, temp, a, index+1);
+        temp.pop_back();
+        solve(res, temp, a, index+1);
+    }
+    vector<vector<int>> subsets(vector<int>& a) {
+        // code here
+        vector<vector<int>>res;
+        vector<int>temp;
+        
+        solve(res, temp, a, 0);
+        sort(res.begin(), res.end());
+        return res;
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-	int t;
-	cin >> t;
+int main() {
+    int t;
+    cin >> t;
 
-	while (t--)
-	{
-		int n, x;
-		cin >> n;
+    while (t--) {
+        int n, x;
+        cin >> n;
 
-		vector<int> array;
-		for (int i = 0; i < n; i++)
-		{
-			cin >> x;
-			array.push_back(x);
-		}
-        
-        
+        vector<int> array;
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+            array.push_back(x);
+        }
+
         Solution ob;
-		vector<vector<int> > res = ob.subsets(array);
+        vector<vector<int> > res = ob.subsets(array);
 
-		// Print result
-		for (int i = 0; i < res.size(); i++) {
-			for (int j = 0; j < res[i].size(); j++)
-				cout << res[i][j] << " ";
-			cout << endl;
-		}
+        // Print result
+        for (int i = 0; i < res.size(); i++) {
+            for (int j = 0; j < res[i].size(); j++)
+                cout << res[i][j] << " ";
+            cout << endl;
+        }
+    }
 
-		
-	}
-
-	return 0;
+    return 0;
 }
 // } Driver Code Ends
