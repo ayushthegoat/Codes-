e193205da3ab7a32eae7c5dd56c2d823a3c448e9
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-16 17:43:15 +0530

    Time: 104 ms (46.08%) | Memory: 46.4 MB (74.91%) - LeetSync

---
+++ 46-permutations/permutations.cs
@@ -0,0 +1,27 @@
+public class Solution {
+   
+private void Swap(ref int a, ref int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+   private void solve(List<IList<int>> result, int n, int index, int[] nums) {
+     if (index >= n) {
+        result.Add(new List<int>(nums));
+        return;
+    }
+    for(int i=index; i<n; i++) {
+        Swap(ref nums[index], ref nums[i]);
+        solve(result, n, index + 1, nums); // Increment index
+        Swap(ref nums[index], ref nums[i]);
+    }
+     
+}
+
+public IList<IList<int>> Permute(int[] nums) {
+    int n = nums.Length;
+    var result = new List<IList<int>>();
+    solve(result, n, 0, nums);
+    return result;
+}
+}
