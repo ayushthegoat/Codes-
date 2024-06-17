8093edb6b9c15387be40117eed455c4e433ac13c
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-17 23:05:36 +0530

    Time: 861 ms (5.04%) | Memory: 148.2 MB (23.78%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cs
+++ 131-palindrome-partitioning/palindrome-partitioning.cs
@@ -12,20 +12,22 @@ public class Solution {
     }
     return true; // Change false to true
 }
-    private void solve(List<IList<string>> result, string s, int index, List<string> temp) {
-         if(index == s.Length) {
-        result.Add(new List<string>(temp));
-        return;
-    }
-
-    for(int i = index; i < s.Length; i++) {
-        string substring = s.Substring(index, i - index + 1);
-        if(isValid(substring)) {
-            temp.Add(substring);
-            solve(result, s, i + 1, temp);
-            temp.RemoveAt(temp.Count - 1);
+    private void solve(List<IList<string>> result, string s, int index, List<string> temo) {
+        if(index == s.Length) {
+            result.Add(new List<string>(temo));
+            return;
         }
-    }
+
+        for(int i=index; i<s.Length; i++) {
+            string substring = s.Substring(index, i-index+1);
+            if(isValid(substring)) {
+                string c = substring;
+                temo.Add(c);
+                solve(result, s, i+1, temo);
+                temo.RemoveAt(temo.Count - 1);
+
+            }
+        }
     }
     public IList<IList<string>> Partition(string s) {
         var result = new List<IList<string>>();
