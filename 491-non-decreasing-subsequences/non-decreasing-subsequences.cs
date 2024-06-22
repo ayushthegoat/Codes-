8cd7d1127496bc354cd80029b8b2e1dcd4a56482
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-22 06:56:09 +0530

    Time: 153 ms (52.94%) | Memory: 77.8 MB (23.53%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cs
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cs
@@ -1,21 +1,33 @@
 public class Solution {
      private void Solve(List<IList<int>> result, int[] nums, int index, List<int> temp, HashSet<string> set) {
-        if (temp.Count >= 2) {
+        if(index>= nums.Length){
+          if(temp.Count >= 2) {
             string key = string.Join(",", temp);
-            if (!set.Contains(key)) {
+            if(!set.Contains(key)) {
                 result.Add(new List<int>(temp));
                 set.Add(key);
             }
+          }
+          return;
         }
 
-        for (int i = index; i < nums.Length; i++) {
-            if (temp.Count == 0 || nums[i] >= temp[temp.Count - 1]) {
-                temp.Add(nums[i]);
-                Solve(result, nums, i + 1, temp, set);
-                temp.RemoveAt(temp.Count - 1);
-            }
+        //   for(int i = index; i<nums.Length; i++) {
+        //     if(temp.Count == 0 || nums[i] >= temp[temp.Count - 1]) {
+        //         temp.Add(nums[i]);
+        //         Solve(result, nums, i+1, temp, set);
+        //         temp.RemoveAt(temp.Count - 1);
+
+        //     }
+        //   }
+
+        if(temp.Count == 0 || nums[index] >= temp[temp.Count - 1]){
+            temp.Add(nums[index]);
+            Solve(result, nums, index+1, temp, set);
+            temp.RemoveAt(temp.Count - 1);
         }
-    }
+            Solve(result, nums, index+1, temp, set);
+
+     }
 
     public IList<IList<int>> FindSubsequences(int[] nums) {
         var result = new List<IList<int>>();
