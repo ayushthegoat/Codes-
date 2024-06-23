0429429aea244faeb8c994571edb01a7a61d76a5
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-23 08:43:40 +0530

    Time: 1316 ms (7.14%) | Memory: 134.8 MB (7.14%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cs
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cs
@@ -3,12 +3,18 @@ public class Solution {
         long[,] dp = new long[nums.Length + 1, 2];
 
 
-        for(int i = 1; i<=nums.Length; i++){
+        for(int i = 1; i<nums.Length + 1; i++){
             //for even length subsequence
             dp[i, 0] = Math.Max(dp[i - 1, 1] - nums[i - 1], dp[i - 1, 0]);
 
             //for odd length subsequence
             dp[i, 1] = Math.Max(dp[i - 1, 0] + nums[i - 1], dp[i - 1, 1]);
+        }
+        for(int i=0;i<dp.GetLength(0);i++) {
+            for(int j=0;j<dp.GetLength(1);j++) {
+                Console.WriteLine(dp[i, j] + " ");
+            }
+            Console.WriteLine();
         }
 
         return Math.Max(dp[nums.Length, 0], dp[nums.Length, 1]);
