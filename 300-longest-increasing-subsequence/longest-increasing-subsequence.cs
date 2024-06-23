704fd2cc9d8a3ceddf3c049d4ddaad8e5e271834
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-23 10:04:34 +0530

    Time: 122 ms (24.34%) | Memory: 42.1 MB (64.06%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
@@ -33,7 +33,7 @@ public class Solution {
            }
            
            int maxLis = 1;
-           for(int i=0;i<n;i++){
+           for(int i=1;i<n;i++){
                for(int j=0;j<i;j++){
                    if(a[i] > a[j]) {
                        dp[i] = Math.Max(dp[i], dp[j] + 1);
