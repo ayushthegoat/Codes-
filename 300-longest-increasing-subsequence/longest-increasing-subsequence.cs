de4fe46aa8f2097d864a6b999cb16260cfe88254
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-23 09:58:25 +0530

    Time: 109 ms (52.96%) | Memory: 41.8 MB (89.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
@@ -1,38 +1,46 @@
 public class Solution {
-    private int n;
-    private int[,] memo;
-    private int Solve(int[] nums, int index, int prev) {
-        if(index >= n) {
-            return 0;
-        }
+    // private int n;
+    // private int[,] memo;
+    // private int Solve(int[] nums, int index, int prev) {
+    //     if(index >= n) {
+    //         return 0;
+    //     }
 
-        int memoIndex = prev + 1;
-        if (memo[index, memoIndex] != -1) {
-            return memo[index, memoIndex];
-        }
+    //     int memoIndex = prev + 1;
+    //     if (memo[index, memoIndex] != -1) {
+    //         return memo[index, memoIndex];
+    //     }
 
-        int take = 0;
-        if(prev == -1 || nums[index] > nums[prev]) {
-            take = 1 + Solve(nums, index + 1, index);
-        }
-        int skip = Solve(nums, index+1, prev);
+    //     int take = 0;
+    //     if(prev == -1 || nums[index] > nums[prev]) {
+    //         take = 1 + Solve(nums, index + 1, index);
+    //     }
+    //     int skip = Solve(nums, index+1, prev);
         
-        memo[index, memoIndex] = Math.Max(take, skip);
+    //     memo[index, memoIndex] = Math.Max(take, skip);
         
-        return memo[index, memoIndex];
+    //     return memo[index, memoIndex];
 
 
-    }
+    // }
 
-    public int LengthOfLIS(int[] nums) {
-        n = nums.Length;
-        memo = new int[n, n+1];
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n+1;j++){
-                memo[i, j] = -1;
-            }
-        }
-        return Solve(nums, 0, -1);
+    public int LengthOfLIS(int[] a) {
+       int n = a.Length;
+       // your code here
+           int[] dp = new int[n];
+           for(int i=0;i<n;i++){
+              dp[i] = 1;
+           }
+           
+           int maxLis = 1;
+           for(int i=0;i<n;i++){
+               for(int j=0;j<i;j++){
+                   if(a[i] > a[j]) {
+                       dp[i] = Math.Max(dp[i], dp[j] + 1);
+                       maxLis = Math.Max(maxLis, dp[i]);
+                   }
+               }
+           }
+           return maxLis;
     }
 }
