477a4a93cb0b33beb1135ddff6dacfb65eb543b8
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-26 19:37:33 +0530

    Time: 89 ms (54.37%) | Memory: 48 MB (45.80%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int max = int.MinValue;
+
+    private int Solve(TreeNode root) {
+        if(root == null) return 0;
+
+
+        int leftMax = Math.Max(0, Solve(root.left));
+        int rightMax = Math.Max(0, Solve(root.right));
+
+        max = Math.Max(max, leftMax + rightMax + root.val);
+
+        return root.val + Math.Max(leftMax, rightMax);
+    }
+
+    public int MaxPathSum(TreeNode root) {
+        if(root == null) return 0;
+
+        Solve(root);
+        return max;
+    }
+}
