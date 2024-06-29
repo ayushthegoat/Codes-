b27478f281988636e932ce7f4ad494743bf95c95
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-06-29 12:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (7.53%) - LeetSync

--- 393-utf-8-validation/utf-8-validation.cpp
+++ 393-utf-8-validation/utf-8-validation.cpp
@@ -5,31 +5,33 @@ public:
         cin.tie(0);
         cout.tie(0);
 
-         int rbytes = 0;
+        int rbytes = 0;
 
-    for (const auto& it : data) {
-        if (rbytes == 0) {
-            if ((it >> 7) == 0) {
-                rbytes = 0;
-            } else if ((it >> 5) == 0b110) {
-                rbytes = 1;
-            } else if ((it >> 4) == 0b1110) {
-                rbytes = 2;
-            } else if ((it >> 3) == 0b11110) {
-                rbytes = 3;
+        for(const auto &it : data) {
+            if(rbytes == 0) {
+                if((it >> 7) == 0) {
+                    rbytes = 0;
+                } else if ((it >> 5) == 0b110) {
+                    rbytes = 1;
+                } else if((it >> 4) == 0b1110) {
+                    rbytes = 2;
+                } else if((it >> 3) == 0b11110) {
+                    rbytes = 3;
+                } else {
+                    return false;
+                }
             } else {
-                return false; // Invalid UTF-8 start byte
-            }
-        } else {
-            if ((it >> 6) == 0b10) {
-                rbytes--;
-            } else {
-                return false; // Invalid UTF-8 continuation byte
+                if((it >> 6) == 0b10) {
+                    rbytes--;
+                } else {
+                    return false;
+                }
             }
         }
-    }
+        if(rbytes == 0) {
+            return true;
+        }
 
-    // If all bytes are properly terminated
-    return rbytes == 0;
+        return false;
     }
 };
