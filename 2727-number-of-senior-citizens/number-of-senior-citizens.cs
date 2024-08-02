400e2c850c973f44d3696145871afd4d00e15869
ayushthegoat <98512726+ayushthegoat@users.noreply.github.com>
2024-08-02 20:55:05 +0530

    Time: 62 ms (68.04%) | Memory: 42.1 MB (12.37%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cs
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cs
@@ -1,11 +1,7 @@
 public class Solution {
     public int CountSeniors(string[] details) {
-        int counter = 0;
-        foreach(var item in details) {
-            string str = item;
-            int age = Convert.ToInt32(str.Substring(11, 2));
-            if(age > 60) counter++;
-        }
-        return counter;
+       return details
+        .Select(item => Convert.ToInt32(item.Substring(11, 2)))
+        .Count(age => age > 60);
     }
 }
